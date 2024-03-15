53dc2f211978c57a9982074203a2492be5f12777
ayshakanta <ayshahussain291@gmail.com>
2024-03-16 03:59:28 +0600

    added json data

---
+++ src/components/Recipes/Recipes.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const Recipes = () => {
+
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() =>{
+        fetch('recipes.json')
+        .then(res => res.json())
+        .then(data => setRecipes(data))
+
+    }, [])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Recipes;
